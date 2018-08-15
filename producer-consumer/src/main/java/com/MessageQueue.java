c83f23490e3aeee9f202925bd313c7d22976f36f
chenkunzetyun  <chenkun@zetyun.com>
2018-08-15 10:03:23 +0800

    add

---
+++ producer-consumer/src/main/java/com/MessageQueue.java
@@ -0,0 +1,20 @@
+package com;
+
+import com.consumer.Consumer;
+import com.producer.Producer;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class MessageQueue {
+    public static void main(String[] args) {
+        //创建队列
+        BlockingQueue<String> queue = new ArrayBlockingQueue<String>(5);
+
+        //创建生产者
+        new Thread(new Producer(queue)).start();
+        //创建消费者
+        new Thread(new Consumer(queue)).start();
+    }
+
+}
---
+++ producer-consumer/src/main/java/com/consumer/Consumer.java
@@ -0,0 +1,41 @@
+package com.consumer;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Consumer implements Runnable {
+
+    private BlockingQueue<String> queue;
+
+    public Consumer(BlockingQueue<String> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        //创建线程池
+        ExecutorService service = Executors.newFixedThreadPool(12);
+        //创建12个消费者
+        for(int i=0;i<4;i++){
+            Future<String> future = service.submit(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    String element = queue.take();
+                    return "消费者消费："+element;
+                }
+            });
+            try {
+                System.out.println(future.get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        service.shutdown();
+    }
+}
